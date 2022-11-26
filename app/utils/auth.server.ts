d7c57c24024b041e8b5190f2b62531a9add177e4
Sami Juntunen <114816867+samiblink@users.noreply.github.com>
2022-11-26 12:58:04 +0200

    first commit

---
+++ app/utils/auth.server.ts
@@ -0,0 +1,65 @@
+import { prisma } from "./prisma.server"
+import { json, createCookieSessionStorage, redirect } from "@remix-run/node"
+import { createUser } from "./user.server"
+import type { RegisterForm, LoginForm } from './types.server'
+import bcrypt from 'bcryptjs'
+
+
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+    throw new Error("SESSION_SECRET must be set")
+}
+// creates a new session. Sets the userID of that session to the id of the logged in user
+// redirects the user to a route you can specify when calling this function.
+// comits the session when setting the cookie header
+const storage = createCookieSessionStorage({
+    cookie: {
+        name: "blink-session",
+        secure: process.env.NODE_ENV === "production",
+        secrets: [sessionSecret],
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
+    }
+})
+export async function createUserSession(userId: string, redirectTo: string) {
+    const session = await storage.getSession()
+    session.set("userId", userId)
+    return redirect(redirectTo, {
+        headers: {
+            "Set-Cookie": await storage.commitSession(session),
+        },
+    })
+}
+export async function register(user: RegisterForm) {
+    const exists = await prisma.user.count({ where: { email: user.email}});
+    if (exists) {
+        return json({ error: "User already exists with that email"}, 
+                    {status: 400})
+    }
+
+    const newUser = await createUser(user)
+    if (!newUser) {
+        return json(
+            {
+                error: "Something went wrong trying to create a new user",
+                fields: { email: user.email, password: user.password },
+            },
+            {   status: 400},
+        )
+    }
+    return createUserSession(newUser.id, "/")
+}
+export async function login({ email, password }: LoginForm) {
+    const user = await prisma.user.findUnique({
+        where: {email},
+    })
+
+    if (!user || !(await bcrypt.compare(password, user.password)))
+        return json(
+            {
+                error: `Incorrect login` }, { status: 400 })
+
+    return  createUserSession(user.id, "/")
+}
